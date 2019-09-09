4aa91d8d22fbfa04a64eaadd23553b000a75999d
jrvaughn <jrvaughn@iastate.edu>
2019-09-09 18:31:48 -0500

    Added a few experiments.

---
+++ Experiments/Jake/AndroidStudioProjects/Login/app/src/main/java/edu/iastate/login/MainActivity.java
@@ -0,0 +1,64 @@
+package edu.iastate.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userNameEditText;
+    private EditText passwordEditText;
+    private Button loginbtn;
+    private Button signUpBtn;
+    private int loginAttemps = 5;
+    private TextView loginAttempsTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userNameEditText = findViewById(R.id.userNameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginbtn = findViewById(R.id.loginbtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        loginAttempsTextView = findViewById(R.id.loginAttempsTextView);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(userNameEditText.getText().toString(), passwordEditText.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName == "Admin") && (userPassword == "pass")){
+
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }else{
+
+            loginAttemps--;
+            loginAttempsTextView.setText("Incorrect User Name or Password" + "\n" +
+                    "Login Attemps Left: " + loginAttemps);
+
+            if (loginAttemps == 0){
+                loginbtn.setEnabled(false);
+                int i = 0;
+                while(i < 1000000){
+                    i++;
+                }
+                loginbtn.setEnabled(true);
+
+            }
+        }
+    }
+}
---
+++ Experiments/Jake/AndroidStudioProjects/Test1/Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView hellotxt = (TextView) findViewById(R.id.Hellotxt);
+                hellotxt.setText("Hello World!");
+            }
+        });
+    }
+}
