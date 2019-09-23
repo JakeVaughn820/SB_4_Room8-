42fda4684f007bc2b5307067dc3513bdeebf3f21
jrvaughn <jrvaughn@iastate.edu>
2019-09-22 23:26:30 -0500

    Volley work.

--- Frontend/app/src/main/java/edu/iastate/room8/ListActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/ListActivity.java
@@ -2,16 +2,19 @@ package edu.iastate.room8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.RequestQueue;
+
 public class ListActivity extends AppCompatActivity {
 
     private Button newList;
     private EditText newListName;
+    private Button btn_back;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class ListActivity extends AppCompatActivity {
 
         newList = findViewById(R.id.newList);
         newListName = findViewById(R.id.newListName);
+        btn_back = findViewById(R.id.btn_back);
 
         newList.setOnClickListener(new View.OnClickListener() {
             @Override
