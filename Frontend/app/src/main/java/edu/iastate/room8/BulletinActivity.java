b7b1a20d8897b092d5a7ce54cd98879d719298df
Paul Degnan <pndegnan@iastate.edu>
2019-09-25 21:45:55 -0500

    Added jsonRequest activity to BulletinActivity

--- Frontend/app/src/main/java/edu/iastate/room8/BulletinActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/BulletinActivity.java
@@ -5,14 +5,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class BulletinActivity extends AppCompatActivity {
     private TextView textView;
+    private RequestQueue mQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bulletin);
+        mQueue = Volley.newRequestQueue(this);
         textView = findViewById(R.id.textView);
-
+        jsonParse();
 
     }
+    private void jsonParse() {
+        String url = "https://api.myjson.com/bins/rlimp";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("List");
+
+                            for (int i = 0; i < jsonArray.length(); i++){
+                                JSONObject List = jsonArray.getJSONObject(i);
+
+                                String id = List.getString("id");
+                                String contents = List.getString("contents");
+                                String dateCreate = List.getString("dateCreate");
+
+                                textView.append("* " + contents + "\n");
+                            }
+                        } catch (JSONException e) {
+                            textView.setText("Something went wrong!>!>!>");
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+
 }
