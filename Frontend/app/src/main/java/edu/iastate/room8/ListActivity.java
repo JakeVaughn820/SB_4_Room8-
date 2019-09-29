35aa21b5a9c56ab585e980ad16d03eb3fe18775e
Paul Degnan <pndegnan@iastate.edu>
2019-09-29 17:00:41 -0500

    Added new list activity that it goes to once clicking on a main list item

--- Frontend/app/src/main/java/edu/iastate/room8/ListActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/ListActivity.java
@@ -2,12 +2,8 @@ package edu.iastate.room8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,7 +12,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,45 +19,29 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
+
+    private TextView titleForList;
 
     private RequestQueue mQueue;
-    private TextView Text_View_List;
-    private Button btn_new_list;
 
     private ListView itemsList;
 
     private ArrayList<String> items;
     private ArrayAdapter<String> adapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_list);
+        setContentView(R.layout.activity_list);
+        String title = getIntent().getStringExtra("EXTRA_INFORMATION");
+        titleForList = findViewById(R.id.TitleForList);
+        itemsList = findViewById(R.id.ListActivityList);
 
-        btn_new_list = findViewById(R.id.btn_create_new_list);
-        mQueue = Volley.newRequestQueue(this);
-        itemsList = findViewById(R.id.itemsList);
+        titleForList.setText(title);
 
-        btn_new_list.setText("+"); //added this as a fix to the + not displaying
-        items = new ArrayList<String>();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-        itemsList.setAdapter(adapter);
         jsonParse();
-
-        //TODO might try and put an on click listener to be able to go into the list and look at the list
-        //TODO or just add sublist functionality, not sure which Thane wants
-
-        btn_new_list.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainListActivity.this, NewListActivity.class);
-                startActivity(i);
-            }
-        });
-
-        itemsList.setOnItemClickListener(messageClickedHandler);
-
     }
 
     private void jsonParse() {
@@ -101,12 +80,4 @@ public class MainListActivity extends AppCompatActivity {
 
         mQueue.add(request);
     }
-    private AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView parent, View v, int position, long id) {
-            Intent i = new Intent(MainListActivity.this, ListActivity.class);
-            i.putExtra("EXTRA_INFORMATION", items.get(position));
-            startActivity(i);
-        }
-    };
-
 }
