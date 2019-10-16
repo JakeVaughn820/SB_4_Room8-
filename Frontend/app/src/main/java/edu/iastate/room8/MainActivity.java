c4ce2866d5167b0b052a6d950055bfe8f8cbb576
Paul Degnan <pndegnan@iastate.edu>
2019-10-15 19:38:40 -0500

    Added post for login

--- Frontend/app/src/main/java/edu/iastate/room8/MainActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/MainActivity.java
@@ -4,10 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.iastate.room8.app.AppController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private Button signUpBtn;
     private int loginAttemps = 5;
     private TextView loginAttempsTextView;
+    private String TAG = NewListActivity.class.getSimpleName();
+
+    // These tags will be used to cancel the requests
+    private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +87,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //TODO put post/parse that will see if email and password is correct
-    //TODO possibly take in the entire users stuff and parse through it? seems too much but easy
+    private void postRequest() {
+        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/listadd";
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("Email", userNameEditText.getText().toString());
+        params.put("Password", passwordEditText.getText().toString());
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) { //TODO Use this same method in lists to update correctly and in bulletin!
+                        Log.d(TAG, response.toString());
+                        try {
+                            String success = response.getString("Success");
+                            //TODO use Jake's validate with success are argument. Change to one parameter.
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Email", userNameEditText.getText().toString());
+                params.put("Password", passwordEditText.getText().toString());
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+    }
+
 }
