5bff2ad84f1781c3e5a1adafa2996289d6d73623
Paul Degnan <pndegnan@iastate.edu>
2019-10-20 16:27:54 -0500

    Created user rooms page

---
+++ Frontend/app/src/main/java/edu/iastate/room8/NewUserRoomJoin.java
@@ -0,0 +1,14 @@
+package edu.iastate.room8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewUserRoomJoin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_room_join);
+    }
+}
