fa6b70343906ae25017b6d6951af5eb4334d1804
Paul Degnan <pndegnan@iastate.edu>
2019-10-21 14:57:17 -0500

    Added comments

--- Frontend/app/src/test/java/edu/iastate/room8/PaulMockitoTest.java
+++ Frontend/app/src/test/java/edu/iastate/room8/PaulMockitoTest.java
@@ -21,38 +21,47 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.runners.MockitoJUnitRunner;
 
+/**
+ * PaulMockitoTest
+ * Tests using Mockito for the project
+ * @author pndegnan
+ */
 public class PaulMockitoTest {
+    /**
+     * Used so Mockito can be used in JUNIT tests
+     */
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
+    /**
+     * Simple JUNIT Test that makes sure that the class PaulMockitoTest is working
+     * If it is not working it will give an error
+     */
     @Test
     public void simpleJUNITTest() {
         assertEquals(1, 1);
     } //Makes sure class works as expected
 
+
+    /**
+     * Used to test schedule, specifically this one tests if the callDateParser method is working
+     * If callDateParser is not working it will not be true. Simple to make sure the method
+     * works and specifically that mocking works.
+     * @throws JSONException
+     */
     @Test
     public void scheduleTest() throws JSONException {
         //This creates a Mock Object of the class that we have not fully implemented
         ScheduleActivity test = mock(ScheduleActivity.class);
         JsonParser jsonParser = new JsonParser();
-        //These are the parameters to the function that we have not yet implemented in the class LoginHandler
+
         String day = "12";
         String month = "14";
         String year = "1998";
         String date = "12/14/1998";
 
-        //We are expecting a JSONObject response from the server that represents whether or not the username matches the password
         when(test.callDateParser()).thenReturn(date);
-    /*In this simulated instance, the response from the server is a JSONObject "loginSuccess",
-    with a boolean value "true" because the login was a success
-     */
-        Assert.assertEquals(date, test.callDateParser());
-    /*This line specifies the behavior of the getResponse method from LoginHandler that currently returns null
-        You can think of it as overriding the behavior of the function and forcing it to return a specific value
-        In the following line, we are forcing this unimplemented method to return our predefined variable "response"
-     */
-        //when(test.getResponse(usernameCorrect,passwordCorrect)).thenReturn(response);
 
-        //Assert.assertEquals(testLogInSuccess.tryLogin(usernameCorrect,passwordCorrect,test),response.getBoolean("loginSuccess"));
+        Assert.assertEquals(date, test.callDateParser());
     }
 }
