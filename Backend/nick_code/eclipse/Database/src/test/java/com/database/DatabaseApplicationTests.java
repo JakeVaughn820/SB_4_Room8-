88e2e0e9b79afc63d4c81b26bc0e3b610a87a7e5
nickisu <nickisu@iastate.edu>
2019-10-21 20:06:29 -0500

    Working on Mockito tests for backend

--- Backend/nick_code/eclipse/Database/src/test/java/com/database/DatabaseApplicationTests.java
+++ Backend/nick_code/eclipse/Database/src/test/java/com/database/DatabaseApplicationTests.java
@@ -1,16 +1,37 @@
 package com.database;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.database.user.User;
+import com.database.user.UserRepository;
+import com.database.user.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DatabaseApplicationTests {
 
+	@Autowired
+	private UserService userService;
+	@MockBean
+	private UserRepository userRepository; 
+	
 	@Test
-	public void contextLoads() {
+	public void getUsersTest() 
+	{
+		when(userRepository.findAll()).thenReturn(Stream.of(new User("Nick", "nickisu@iastate.edu", "password"), 
+				new User("Thane", "Thane@iastate.edu", "password")).collect(Collectors.toList()));
+		assertEquals(2, userService.getUsers().size());
 	}
 
 }
