7df7fabe28128d2848350af77d994982b884ae71
jrvaughn <jrvaughn@iastate.edu>
2019-10-22 19:23:26 -0500

    First test works same problem with the second as the first but this time more complex

--- Frontend/app/src/test/java/edu/iastate/room8/SessionsTest.java
+++ Frontend/app/src/test/java/edu/iastate/room8/SessionsTest.java
@@ -7,8 +7,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.MockitoRule;
 import android.content.Context;
 
@@ -19,33 +22,48 @@ import edu.iastate.room8.utils.SessionManager;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SessionsTest {
+    @Mock
+    Context mockContext;
+    @Mock
+    SharedPreferences mockPrefs;
+    @Mock
+    SharedPreferences.Editor mockEditor;
 
-    SharedPreferences sharedPrefs;
-    SharedPreferences.Editor editor;
-    Context context;
-    SessionManager sessionManager;
-
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    private SessionManager sessionManager;
 
     @Before
     public void before() throws Exception {
-        this.sharedPrefs = Mockito.mock(SharedPreferences.class);
-        this.context = Mockito.mock(Context.class);
-        Mockito.when(context.getSharedPreferences(anyString(), anyInt())).thenReturn(sharedPrefs);
+
+        Mockito.when(mockContext.getSharedPreferences(anyString(), anyInt())).thenReturn(mockPrefs);
+        Mockito.when(mockContext.getSharedPreferences(anyString(), anyInt()).edit()).thenReturn(mockEditor);
+
+        sessionManager = new SessionManager(mockContext);
+    }
+
+    @Test
+    public void getUserDetailTest() {
+        Mockito.when(mockPrefs.getString("NAME", null)).thenReturn("Joe");
+        Mockito.when(mockPrefs.getString("EMAIL", null)).thenReturn("Joe@email.com");
+        Mockito.when(mockPrefs.getString("ID", null)).thenReturn("34");
+        Mockito.when(mockPrefs.getString("ROOM", null)).thenReturn("8");
+
+        assertEquals("Joe", sessionManager.getUserDetail().get("NAME"));
+        assertEquals("Joe@email.com", sessionManager.getUserDetail().get("EMAIL"));
+        assertEquals("34", sessionManager.getUserDetail().get("ID"));
+        assertEquals("8", sessionManager.getUserDetail().get("ROOM"));
 
     }
 
-
     @Test
-    public void createSessionTest() throws JSONException {
-        sessionManager = new SessionManager(context);
-        Mockito.when(sharedPrefs.getString(anyString(), anyString())).thenReturn("foobar");
+    public void createSessionTest() {
+        sessionManager.createSession("Jack", "Jack@email.com", "35");
 
-
-        assertEquals("Joe", sessionManager.getUserDetail().get("NAME"));
-
+        assertEquals("Jack", sessionManager.getUserDetail().get("NAME"));
+        assertEquals("Jack@email.com", sessionManager.getUserDetail().get("EMAIL"));
+        assertEquals("35", sessionManager.getUserDetail().get("ID"));
+        assertEquals(null, sessionManager.getUserDetail().get("ROOM"));
     }
 }
 
