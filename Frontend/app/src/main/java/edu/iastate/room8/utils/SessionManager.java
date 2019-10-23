1ea9e9a6409a3eefae89345a52c81cb6c5b0890e
jrvaughn <jrvaughn@iastate.edu>
2019-10-23 03:44:36 -0500

    Added comments to sessions and more code, but mostly comments

--- Frontend/app/src/main/java/edu/iastate/room8/utils/SessionManager.java
+++ Frontend/app/src/main/java/edu/iastate/room8/utils/SessionManager.java
@@ -33,6 +33,7 @@ public class SessionManager {
         editor = sharedPreferences.edit();
     }
 
+    //Creates a new session with everything set to null other than the params.
     public void createSession(String name, String email, String id){
 
         editor.putBoolean(LOGIN, true);
@@ -44,6 +45,7 @@ public class SessionManager {
         editor.apply();
     }
 
+    //Adds a room to ROOMS and sets it as current room.
     public void addRoom (String room){
         Set<String> set = new HashSet<>();
         set = (sharedPreferences.getStringSet(ROOMS, null));
@@ -53,12 +55,15 @@ public class SessionManager {
         editor.apply();
     }
 
+    //True if user is currently in a room false if otherwise
     public boolean isRoom (String room){
         Set<String> set;
         set = (sharedPreferences.getStringSet(ROOMS, null));
 
         return set.contains(room);
     }
+
+    //Sets the current room the user is in.
     public void setRoom (String room){
         if(isRoom(room)){
             editor.putString(ROOM, room);
@@ -66,6 +71,7 @@ public class SessionManager {
         }
     }
 
+    //Returns true if the user is in a room false if otherwise
     public boolean isInRoom(){
         if (sharedPreferences.getString(ROOM, null) != null){
             return true;
@@ -73,6 +79,7 @@ public class SessionManager {
         return false;
     }
 
+    //Checks Room to see if the user is in a room if not it puts user on NewUserRoomJoin
     public void checkRoom(){
         if (!this.isInRoom()){
             Intent i = new Intent(context, NewUserRoomJoin.class);
@@ -81,26 +88,37 @@ public class SessionManager {
         }
     }
 
+    //Returns current room if user is in room
     public String getRoom(){
         return sharedPreferences.getString(ROOM, null);
     }
 
+    //Returns all the rooms the user is a part of.
+    public Set<String> getRooms(){
+        return sharedPreferences.getStringSet(ROOMS, null);
+    }
+
+    //Returns the name of the user.
     public String getName(){
         return sharedPreferences.getString(NAME, null);
     }
 
+    //Returns the Email of the user.
     public String getEmail(){
         return sharedPreferences.getString(EMAIL, null);
     }
 
+    //Returns the ID of the user.
     public String getID(){
         return sharedPreferences.getString(ID, null);
     }
 
+    //Returns true if the user is logged in false if otherwise.
     public boolean isLoggin(){
         return sharedPreferences.getBoolean(LOGIN, false);
     }
 
+    //Checks if the user is logged in if they are not logged in go to LoginActivity
     public void checkLogin(){
 
         if (!this.isLoggin()){
@@ -110,6 +128,7 @@ public class SessionManager {
         }
     }
 
+    //Returns all of the users Details as a HashMap<"Keyword", "Thing">
     public HashMap<String, String> getUserDetail(){
 
         HashMap<String, String> user = new HashMap<>();
@@ -117,18 +136,20 @@ public class SessionManager {
         user.put(EMAIL, sharedPreferences.getString(EMAIL, null));
         user.put(ID, sharedPreferences.getString(ID, null));
         user.put(ROOM, sharedPreferences.getString(ROOM, null));
-        user.put(ROOMS, sharedPreferences.getString(ROOMS, null));
 
         return user;
     }
 
+    //Leaves the current room and switches screen to NewUserRoomJoin.
     public void leaveRoom(){
         editor.putString(ROOM, null);
         editor.apply();
-        Intent i = new Intent(context, LoginActivity.class);
+        Intent i = new Intent(context, NewUserRoomJoin.class);
         context.startActivity(i);
         ((HomeActivity) context).finish();
     }
+
+    //Logs out of the session clearing all of the users data from shared preferences.
     public void logout(){
 
         editor.clear();
