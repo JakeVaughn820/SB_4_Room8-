2a12970ac9c6a96780bd482d492fadf803434108
jrvaughn <jrvaughn@iastate.edu>
2019-10-23 15:35:53 -0500

    Pulling from master

--- Frontend/app/src/main/java/edu/iastate/room8/MainActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private void validate(String userName, String userPassword){
         if((userName.equals("")) && (userPassword.equals(""))){
 
-            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+            Intent i = new Intent(MainActivity.this, HomeActivityMoreTab.class);
             startActivity(i);
 
         }else{
