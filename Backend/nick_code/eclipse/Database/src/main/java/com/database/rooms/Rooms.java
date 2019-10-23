b21f94a123898b31554ea1361188fd0a47e28a4b
nickisu <nickisu@iastate.edu>
2019-10-23 02:15:03 -0500

    Updated room_name to Title

--- Backend/nick_code/eclipse/Database/src/main/java/com/database/rooms/Rooms.java
+++ Backend/nick_code/eclipse/Database/src/main/java/com/database/rooms/Rooms.java
@@ -17,12 +17,12 @@ public class Rooms
 	@Column(name="id")
 	private String id; 
 	
-	@Column(name="room_name")
-	private String roomName;
+	@Column(name="title")
+	private String title;
 	
 	public Rooms(String roomName)
 	{
-		this.roomName = roomName; 
+		this.title = roomName; 
 	}
 	
 	public void setId(String id)
@@ -32,7 +32,7 @@ public class Rooms
 	
 	public void setRoomName(String roomName)
 	{
-		this.roomName = roomName; 
+		this.title = roomName; 
 	}
 	
 	public String getId()
@@ -40,9 +40,9 @@ public class Rooms
 		return this.id; 
 	}
 	
-	public String getRoomName()
+	public String getTitle()
 	{
-		return this.roomName; 
+		return this.title; 
 	}
 	
 	@Override 
@@ -53,7 +53,7 @@ public class Rooms
 		if(!(o instanceof Rooms))
 			return false; 
 		Rooms room = (Rooms) o;
-		return this.id == room.id && this.roomName == room.roomName;
+		return this.id == room.id && this.title == room.title;
 	}
 
 }
