e9f0212e5d4c9fd0ab27ce0d22b695c208442b0c
jrvaughn <jrvaughn@iastate.edu>
2019-11-03 15:53:33 -0600

    added websockets to bulletin

--- Frontend/app/src/main/java/edu/iastate/room8/BulletinActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/BulletinActivity.java
@@ -2,6 +2,7 @@ package edu.iastate.room8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
@@ -20,10 +21,14 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -71,6 +76,10 @@ public class BulletinActivity extends AppCompatActivity {
      */
     private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
     /**
+     * mWebSocketClient used for connecting websocket to server.
+     */
+    private WebSocketClient mWebSocketClient;
+    /**
      * session manager used for settings and information for the specific user
      */
     SessionManager sessionManager;
@@ -81,6 +90,7 @@ public class BulletinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bulletin);
 
         sessionManager = new SessionManager(this);
+        connectWebSocket();
 
         mQueue = Volley.newRequestQueue(this);
         textView = findViewById(R.id.textView);
@@ -88,12 +98,12 @@ public class BulletinActivity extends AppCompatActivity {
         toAddText = findViewById(R.id.messageToAdd);
         arr = new ArrayList<String>();
 
-        try{
-            jsonParse();  //Parses through the json given to frontend from back end
-        }catch(JSONException e){
-            textView.setText("Something went wrong!>!>!>");
-            e.printStackTrace();
-        }
+//        try{
+//            jsonParse();  //Parses through the json given to frontend from back end
+//        }catch(JSONException e){
+//            textView.setText("Something went wrong!>!>!>");
+//            e.printStackTrace();
+//        }
 
         toAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,7 +112,8 @@ public class BulletinActivity extends AppCompatActivity {
                 if(stringToAddText.equals("")){
                     Toast.makeText(BulletinActivity.this, "Must input a message to display on the bulletin board", Toast.LENGTH_LONG).show();
                 }else{
-                    postRequest();
+                    //postRequest();
+                    sendMessage(view);
                 }
                 toAddText.setText("");
             }
@@ -115,7 +126,7 @@ public class BulletinActivity extends AppCompatActivity {
      * Receives: Header: BulletinBoard. Keys: User. Contents.
      * @throws JSONException
      */
-    public void jsonParse() throws JSONException {
+//    public void jsonParse() throws JSONException {
 //        String url = "https://api.myjson.com/bins/1g4fnt";
 //        String url = "https://api.myjson.com/bins/k7wvo";
         String url = "https://api.myjson.com/bins/158r6c";
@@ -132,84 +143,84 @@ public class BulletinActivity extends AppCompatActivity {
 //            textView.append(contents + "\n");
 //        }
 //        mQueue.add(jsonParser.returnRequest());
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("BulletinBoard");
-
-                            for (int i = 0; i < jsonArray.length(); i++){
-                                JSONObject List = jsonArray.getJSONObject(i);
-
-                                String id = List.getString("User");
-                                String contents = List.getString("Contents");
-//                                textView.append(Html.fromHtml("<b>"+ id + ": </b>"));
-//                                textView.append(contents + "\n");
-                                String temp = Html.fromHtml("<b>"+ id + ": </b>") + contents + "\n";
-                                arr.add(temp);
-                                appendTextView();
-                            }
-                        } catch (JSONException e) {
-                            textView.setText("Something went wrong!>!>!>");//yml
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
-    }
-
-
-    /**
-     * Used to send a message to the server to put onto the bulletin
-     * Sends information with keys: User, Contents.
-     */
-    private void postRequest() {
-        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/bulletin";
-        url = url + "/" + sessionManager.getRoom();
-
-
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("User", sessionManager.getName());
-        params.put("Contents", stringToAddText);
-        JSONObject toPost = new JSONObject(params);
-//        Toast.makeText(this, toPost.toString(), Toast.LENGTH_SHORT).show();
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-                url, toPost,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d(TAG, response.toString());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/json");
-                return headers;
-            }
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("User", "User");
-                params.put("Contents", stringToAddText);
-
-                return params;
-            }
-        };
-        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
-    }
+//
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        try {
+//                            JSONArray jsonArray = response.getJSONArray("BulletinBoard");
+//
+//                            for (int i = 0; i < jsonArray.length(); i++){
+//                                JSONObject List = jsonArray.getJSONObject(i);
+//
+//                                String id = List.getString("User");
+//                                String contents = List.getString("Contents");
+////                                textView.append(Html.fromHtml("<b>"+ id + ": </b>"));
+////                                textView.append(contents + "\n");
+//                                String temp = Html.fromHtml("<b>"+ id + ": </b>") + contents + "\n";
+//                                arr.add(temp);
+//                                appendTextView();
+//                            }
+//                        } catch (JSONException e) {
+//                            textView.setText("Something went wrong!>!>!>");//yml
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                error.printStackTrace();
+//            }
+//        });
+//        mQueue.add(request);
+//    }
+//
+//
+//    /**
+//     * Used to send a message to the server to put onto the bulletin
+//     * Sends information with keys: User, Contents.
+//     */
+//    private void postRequest() {
+//        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/bulletin";
+//        url = url + "/" + sessionManager.getRoom();
+//
+//
+//        Map<String, String> params = new HashMap<String, String>();
+//        params.put("User", sessionManager.getName());
+//        params.put("Contents", stringToAddText);
+//        JSONObject toPost = new JSONObject(params);
+////        Toast.makeText(this, toPost.toString(), Toast.LENGTH_SHORT).show();
+//        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+//                url, toPost,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        Log.d(TAG, response.toString());
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                VolleyLog.d(TAG, "Error: " + error.getMessage());
+//            }
+//        }) {
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                HashMap<String, String> headers = new HashMap<String, String>();
+//                headers.put("Content-Type", "application/json");
+//                return headers;
+//            }
+//            @Override
+//            protected Map<String, String> getParams() {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("User", "User");
+//                params.put("Contents", stringToAddText);
+//
+//                return params;
+//            }
+//        };
+//        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+//    }
 
     /**
      * Method that will make sure the messages wont go off the screen.
@@ -242,4 +253,51 @@ public class BulletinActivity extends AppCompatActivity {
         }
         return reverse;
     }
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("wss://echo.websocket.org");
+        } catch (
+                URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView textView = (TextView) findViewById(R.id.textView);
+                        textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.messageToAdd);
+        mWebSocketClient.send(editText.getText().toString());
+        editText.setText("");
+    }
+
 }
