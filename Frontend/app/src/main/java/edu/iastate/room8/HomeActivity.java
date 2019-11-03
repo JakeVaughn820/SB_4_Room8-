1bde86c44b7b475029e4454602eccbaab9c5af05
jrvaughn <jrvaughn@iastate.edu>
2019-11-03 15:20:46 -0600

    Enabled room checking.

--- Frontend/app/src/main/java/edu/iastate/room8/HomeActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/HomeActivity.java
@@ -42,7 +42,7 @@ public class HomeActivity extends AppCompatActivity {
 
         sessionManager = new SessionManager(this);
         sessionManager.checkLogin();
-//        sessionManager.checkRoom();
+        sessionManager.checkRoom();
 
         tempButton = findViewById(R.id.tempButton);
         tempButtonBulletin = findViewById(R.id.tempButtonBulletin);
