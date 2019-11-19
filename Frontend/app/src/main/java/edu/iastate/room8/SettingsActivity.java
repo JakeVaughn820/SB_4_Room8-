95e39bb9f5fe05932dfa5ef18b9fd7c6eae093cb
jrvaughn <jrvaughn@iastate.edu>
2019-11-19 13:57:35 -0600

    User settings is now ready for post request for new username email and password.  Added to sessionsmanage for updating name and email.

--- Frontend/app/src/main/java/edu/iastate/room8/SettingsActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/SettingsActivity.java
@@ -1,25 +1,33 @@
 package edu.iastate.room8;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import edu.iastate.room8.utils.SessionManager;
+
 public class SettingsActivity extends AppCompatActivity {
+    static SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+        sessionManager = new SessionManager(this);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -27,7 +35,36 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
+            final EditTextPreference newName, newEmail, newPass;
 
+            newName = findPreference("userName");
+            newEmail = findPreference("userEmail");
+            newPass = findPreference("userPass");
+
+            newName.setText(sessionManager.getName());
+            newEmail.setText(sessionManager.getEmail());
+
+            newName.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    sessionManager.setName(editText.getText().toString());
+                }
+            });
+
+            newEmail.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    sessionManager.setEmail(editText.getText().toString());
+
+                }
+            });
+
+            newPass.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+
+                }
+            });
         }
     }
 }
