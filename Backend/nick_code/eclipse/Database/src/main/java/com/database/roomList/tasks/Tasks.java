22bd85f092b03379a8e47ba14630e6f1415b33d8
nickisu <nickisu@iastate.edu>
2019-11-18 22:13:46 -0600

    Fixed bugs related to seeing what rooms a user is in

--- Backend/nick_code/eclipse/Database/src/main/java/com/database/roomList/tasks/Tasks.java
+++ Backend/nick_code/eclipse/Database/src/main/java/com/database/roomList/tasks/Tasks.java
@@ -1,199 +1,199 @@
-//package com.database.roomList.tasks;
-//
-//import java.util.List;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.FetchType;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.OneToMany;
-//import javax.persistence.Table;
-//
-///**
-// * This class implements the tasks entity. Each roomList can have 
-// * multiple tasks. 
-// * 
-// * @author Nickolas Mitchell
-// */
-//@Entity
-//@Table(name="tasks")
-//public class Tasks 
-//{
+package com.database.roomList.tasks;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * This class implements the tasks entity. Each roomList can have 
+ * multiple tasks. 
+ * 
+ * @author Nickolas Mitchell
+ */
+@Entity
+@Table(name="tasks")
+public class Tasks 
+{
+	/**
+	 * A unique Id which is automatically generated for each task.
+	 */
+	@Id
+	@Column(name = "id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	/**
+	 * Holds the contents of this task.
+	 */
+	@Column(name = "contents")
+	private String contents;
+
+	/**
+	 * Assigns each task to a user. A user can have multiple tasks.
+	 */
+	@ManyToOne(fetch = FetchType.LAZY, targetEntity = com.database.user.User.class)
+	@JoinColumn(name = "user_task_id", referencedColumnName = "id")
+	private List<Long> users;
+
+	/**
+	 * Holds the roomList this task was created in.
+	 */
+	@ManyToOne(fetch = FetchType.LAZY, targetEntity = com.database.roomList.RoomList.class)
+	@JoinColumn(name = "list_id", referencedColumnName = "id")
+	private Long list;
+
 //	/**
-//	 * A unique Id which is automatically generated for each task.
+//	 * SubTask Id's associated with this Task.
 //	 */
-//	@Id
-//	@Column(name = "id", nullable = false)
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private Long id;
-//
+//	@OneToMany(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "subtask_id", referencedColumnName = "id")
+//	private List<Long> subtasks;
+
+	/**
+	 * Default Constructor
+	 */
+	public Tasks() {
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param list
+	 * @param contents
+	 * @param uses
+	 */
+	public Tasks(String contents, Long userid, Long listid)
+	{
+		this.contents = contents;
+		this.users.add(userid);
+		this.list = listid;
+	}
+
+	/**
+	 * Gets the task Id.
+	 * 
+	 * @return
+	 */
+	public Long getId() 
+	{
+		return id;
+	}
+
+	/**
+	 * Gets the task's contents.
+	 * 
+	 * @return
+	 */
+	public String getContents() {
+		return contents;
+	}
+
+	/**
+	 * Gets the users associated with this task.
+	 * 
+	 * @return
+	 */
+	public List<Long> getUser() {
+		return users;
+	}
+
+	/**
+	 * Gets list Id associated with this task.
+	 * 
+	 * @return
+	 */
+	public Long getListId() {
+		return list;
+	}
+
+	/**
+	 * Sets the task Id.
+	 * 
+	 * @param id
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Sets the task's contents.
+	 * 
+	 * @param contents
+	 */
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	/**
+	 * Sets one user Id.
+	 *
+	 * @param userId
+	 */
+	public void setUserId(Long userId) {
+		users.add(userId);
+	}
+
+	/**
+	 * Sets the List Id.
+	 * 
+	 * @param listId
+	 */
+	public void setListId(Long listId) {
+		this.list = listId;
+	}
+
 //	/**
-//	 * Holds the contents of this task.
-//	 */
-//	@Column(name = "contents")
-//	private String contents;
-//
-//	/**
-//	 * Assigns each task to a user. A user can have multiple tasks.
-//	 */
-//	@ManyToOne(fetch = FetchType.LAZY, targetEntity = com.database.user.User.class)
-//	@JoinColumn(name = "user_task_id", referencedColumnName = "id")
-//	private List<Long> users;
-//
-//	/**
-//	 * Holds the roomList this task was created in.
-//	 */
-//	@ManyToOne(fetch = FetchType.LAZY, targetEntity = com.database.roomList.RoomList.class)
-//	@JoinColumn(name = "list_id", referencedColumnName = "id")
-//	private Long list;
-//
-////	/**
-////	 * SubTask Id's associated with this Task.
-////	 */
-////	@OneToMany(fetch = FetchType.LAZY)
-////	@JoinColumn(name = "subtask_id", referencedColumnName = "id")
-////	private List<Long> subtasks;
-//
-//	/**
-//	 * Default Constructor
-//	 */
-//	public Tasks() {
-//	}
-//
-//	/**
-//	 * Constructor
-//	 * 
-//	 * @param list
-//	 * @param contents
-//	 * @param uses
-//	 */
-//	public Tasks(String contents, Long users, Long list)
-//	{
-//		this.contents = contents;
-//		this.users.add(users);
-//		this.list = list;
-//	}
-//
-//	/**
-//	 * Gets the task Id.
+//	 * Gets all subtasks for this task.
 //	 * 
 //	 * @return
 //	 */
-//	public Long getId() 
-//	{
-//		return id;
+//	public List<Long> getSubTasks() {
+//		return subtasks;
 //	}
 //
 //	/**
-//	 * Gets the task's contents.
+//	 * Adds one subtask to this task.
 //	 * 
-//	 * @return
+//	 * @param subtask
 //	 */
-//	public String getContents() {
-//		return contents;
+//	public void setSubTask(Long subtask) {
+//		this.subtasks.add(subtask);
 //	}
-//
-//	/**
-//	 * Gets the users associated with this task.
-//	 * 
-//	 * @return
-//	 */
-//	public List<Long> getUser() {
-//		return users;
-//	}
-//
-//	/**
-//	 * Gets list Id associated with this task.
-//	 * 
-//	 * @return
-//	 */
-//	public Long getListId() {
-//		return list;
-//	}
-//
-//	/**
-//	 * Sets the task Id.
-//	 * 
-//	 * @param id
-//	 */
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	/**
-//	 * Sets the task's contents.
-//	 * 
-//	 * @param contents
-//	 */
-//	public void setContents(String contents) {
-//		this.contents = contents;
-//	}
-//
-//	/**
-//	 * Sets one user Id.
-//	 *
-//	 * @param userId
-//	 */
-//	public void setUserId(Long userId) {
-//		users.add(userId);
-//	}
-//
-//	/**
-//	 * Sets the List Id.
-//	 * 
-//	 * @param listId
-//	 */
-//	public void setListId(Long listId) {
-//		this.list = listId;
-//	}
-//
-////	/**
-////	 * Gets all subtasks for this task.
-////	 * 
-////	 * @return
-////	 */
-////	public List<Long> getSubTasks() {
-////		return subtasks;
-////	}
-////
-////	/**
-////	 * Adds one subtask to this task.
-////	 * 
-////	 * @param subtask
-////	 */
-////	public void setSubTask(Long subtask) {
-////		this.subtasks.add(subtask);
-////	}
-//
-//	/**
-//	 * Returns: "User: " ~~~~ "Contents: " ~~~~~
-//	 */
-//	@Override
-//	public String toString() {
-//		String temp = "";
-//		for (Long i : users) {
-//			temp += "User: " + users.get(Math.toIntExact(i)) + "Contents: " + contents;
-//			temp += "\n";
-//		}
-//		return temp;
-//	}
-//
-//	/**
-//	 * Checks if two tasks are the same.
-//	 */
-//	@Override
-//	public boolean equals(Object o) {
-//		if (o == this)
-//			return true;
-//		if (!(o instanceof Tasks))
-//			return false;
-//		Tasks task = (Tasks) o;
-//		return this.id == task.id && this.contents.equals(task.contents) && this.users.equals(task.users)
-//				&& this.list == task.list; 
-//				//&& this.subtasks.equals(task.subtasks);
-//	}
-//}
+
+	/**
+	 * Returns: "User: " ~~~~ "Contents: " ~~~~~
+	 */
+	@Override
+	public String toString() {
+		String temp = "";
+		for (Long i : users) {
+			temp += "User: " + users.get(Math.toIntExact(i)) + "Contents: " + contents;
+			temp += "\n";
+		}
+		return temp;
+	}
+
+	/**
+	 * Checks if two tasks are the same.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof Tasks))
+			return false;
+		Tasks task = (Tasks) o;
+		return this.id == task.id && this.contents.equals(task.contents) && this.users.equals(task.users)
+				&& this.list == task.list; 
+				//&& this.subtasks.equals(task.subtasks);
+	}
+}
--- Backend/nick_code/eclipse/Database/src/main/java/com/database/roomList/tasks/TasksService.java
+++ Backend/nick_code/eclipse/Database/src/main/java/com/database/roomList/tasks/TasksService.java
@@ -1,67 +1,67 @@
-//package com.database.roomList.tasks;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * This class implements the task repository. 
-// * 
-// * @author Nickolas Mitchell
-// */
-//@Service
-//public class TasksService 
-//{
-//	/**
-//	 * Holds the task repository object. 
-//	 */
-//	@Autowired
-//	private TasksRepository taskRepository; 
-//	
-//	/**
-//	 * Gets all tasks in the database. 
-//	 * 
-//	 * @return
-//	 */
-//	public List<Tasks> getTask() 
-//	{
-//		return taskRepository.findAll();
-//	}
-//	
-//	/**
-//	 * Adds a task to the database. 
-//	 * 
-//	 * @param Task
-//	 * @return
-//	 */
-//	public Tasks addTask(Tasks Task)
-//	{
-//		return taskRepository.save(Task);
-//	}
-//	
-//	/**
-//	 * Gets number of tasks within the database. 
-//	 * 
-//	 * @return
-//	 */
-//    public Long count() 
-//    {
-//        return taskRepository.count();
-//    }
-//
-//    /**
-//     * Deletes a task from the database. Throws IllegalArgumentException 
-//     * if the task does not exist. 
-//     * 
-//     * @param taskId
-//     * @return
-//     * @throws IllegalArgumentException
-//     */
-//    public boolean deleteById(Long taskId) throws IllegalArgumentException
-//    {
-//    	taskRepository.deleteById(taskId);
-//        return true; 
-//    }
-//
-//}
+package com.database.roomList.tasks;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * This class implements the task repository. 
+ * 
+ * @author Nickolas Mitchell
+ */
+@Service
+public class TasksService 
+{
+	/**
+	 * Holds the task repository object. 
+	 */
+	@Autowired
+	private TasksRepository taskRepository; 
+	
+	/**
+	 * Gets all tasks in the database. 
+	 * 
+	 * @return
+	 */
+	public List<Tasks> getTask() 
+	{
+		return taskRepository.findAll();
+	}
+	
+	/**
+	 * Adds a task to the database. 
+	 * 
+	 * @param Task
+	 * @return
+	 */
+	public Tasks addTask(Tasks Task)
+	{
+		return taskRepository.save(Task);
+	}
+	
+	/**
+	 * Gets number of tasks within the database. 
+	 * 
+	 * @return
+	 */
+    public Long count() 
+    {
+        return taskRepository.count();
+    }
+
+    /**
+     * Deletes a task from the database. Throws IllegalArgumentException 
+     * if the task does not exist. 
+     * 
+     * @param taskId
+     * @return
+     * @throws IllegalArgumentException
+     */
+    public boolean deleteById(Long taskId) throws IllegalArgumentException
+    {
+    	taskRepository.deleteById(taskId);
+        return true; 
+    }
+
+}
