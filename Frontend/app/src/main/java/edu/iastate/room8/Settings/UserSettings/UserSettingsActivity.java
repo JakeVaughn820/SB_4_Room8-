ccd6610a38ca9d45894cfc5ed4330bc3865ccc07
Paul Degnan <pndegnan@iastate.edu>
2019-12-10 17:02:43 -0600

    quick fix for usersettings

--- Frontend/app/src/main/java/edu/iastate/room8/Settings/UserSettings/UserSettingsActivity.java
+++ Frontend/app/src/main/java/edu/iastate/room8/Settings/UserSettings/UserSettingsActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -20,7 +21,6 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-import edu.iastate.room8.List.NewListActivity;
 import edu.iastate.room8.R;
 import edu.iastate.room8.app.AppController;
 import edu.iastate.room8.utils.Sessions.SessionManager;
@@ -51,25 +51,13 @@ public class UserSettingsActivity extends AppCompatActivity {
      */
     private String passwordEditTextString;
     /**
-     * String with the user's ID
+     * Current password
      */
-    private String UserID;
-    /**
-     * String with the user's Name
-     */
-    private String UserName; //TODO fix warnings
-    /**
-     * String with the user's email
-     */
-    private String UserEmail;
-    /**
-     * String with the user's password
-     */
-    private String UserPass;
+    private EditText currentPassword;
     /**
      * Tag with the current activity
      */
-    private String TAG = NewListActivity.class.getSimpleName();
+    private String TAG = UserSettingsActivity.class.getSimpleName();
     /**
      * Session Manager variable
      */
@@ -88,25 +76,22 @@ public class UserSettingsActivity extends AppCompatActivity {
         Button btnChangeEamil = findViewById(R.id.btnChangeEmail);
         Button btnChangePass = findViewById(R.id.btnChangePass);
         Button btnlogout = findViewById(R.id.btnLogout);
+        Button changeAll = findViewById(R.id.changeAll);
+        currentPassword = findViewById(R.id.editTextCurrentPassword);
 
         btnChangeName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                UserID = sessionManager.getID();
-                UserName = sessionManager.getName();
-                UserEmail = sessionManager.getEmail();
                 userNameEditTextString = userNameEditText.getText().toString();
                 sessionManager.setName(userEmailEditTextString);
                 postRequestName();
             }
         });
 
+
         btnChangeEamil.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                UserID = sessionManager.getID();
-                UserName = sessionManager.getName();
-                UserEmail = sessionManager.getEmail();
                 userEmailEditTextString = userEmailEditText.getText().toString();
                 sessionManager.setEmail(userEmailEditText.getText().toString());
                 postRequestEmail();
@@ -116,9 +101,6 @@ public class UserSettingsActivity extends AppCompatActivity {
         btnChangePass.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                UserID = sessionManager.getID();
-                UserName = sessionManager.getName();
-                UserEmail = sessionManager.getEmail();
                 passwordEditTextString = passwordEditText.getText().toString();
                 postRequestPass();
             }
@@ -127,18 +109,25 @@ public class UserSettingsActivity extends AppCompatActivity {
         btnlogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO
+                sessionManager.logout();
+            }
+        });
+
+        changeAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postRequestAll();
             }
         });
     }
 
     private void postRequestName() {
-        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/ChangeName";
-        url = url + "/" + sessionManager.getID();
+        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/updateuser";
+        url = url + "/" + sessionManager.getID() + "/";
 
         Map<String, String> params = new HashMap<>();
-        params.put("ID", UserID); //email to register
         params.put("Name", userNameEditTextString); //name to register
+//        params.put("userId", sessionManager.getID());
 
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
                 url, new JSONObject(params),
@@ -148,6 +137,7 @@ public class UserSettingsActivity extends AppCompatActivity {
                         Log.d(TAG, response.toString());
                         try {
                             String success = response.getString("Response");
+                            Toast.makeText(UserSettingsActivity.this, success, Toast.LENGTH_SHORT).show();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -168,12 +158,13 @@ public class UserSettingsActivity extends AppCompatActivity {
     }
 
     private void postRequestEmail() {
-        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/ChangeEmail";
-        url = url + "/" + sessionManager.getID();
+        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/updateuser";
+        url = url + "/" + sessionManager.getID() + "/";
 
         Map<String, String> params = new HashMap<>();
-        params.put("ID", UserID); //password to register
         params.put("Email", userEmailEditTextString); //email to register
+//        params.put("userId", sessionManager.getID());
+
 
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
                 url, new JSONObject(params),
@@ -183,6 +174,7 @@ public class UserSettingsActivity extends AppCompatActivity {
                         Log.d(TAG, response.toString());
                         try {
                             String success = response.getString("Response");
+                            Toast.makeText(UserSettingsActivity.this, success, Toast.LENGTH_SHORT).show();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -202,12 +194,16 @@ public class UserSettingsActivity extends AppCompatActivity {
         AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
     }
     private void postRequestPass() {
-        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/ChangePass";
-        url = url + "/" + sessionManager.getID();
+        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/updateuser";
+        url = url + "/" + sessionManager.getID() + "/";
 
         Map<String, String> params = new HashMap<>();
-        params.put("ID", UserID); //name to register
         params.put("Password", passwordEditTextString); //password to register
+        params.put("CurrentPassword", currentPassword.getText().toString());
+
+
+//        params.put("userId", sessionManager.getID());
+
 
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
                 url, new JSONObject(params),
@@ -217,6 +213,53 @@ public class UserSettingsActivity extends AppCompatActivity {
                         Log.d(TAG, response.toString());
                         try {
                             String success = response.getString("Response");
+                            Toast.makeText(UserSettingsActivity.this, success, Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) { //on error for json
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        }) {
+
+        };
+        //These tags will be used to cancel the requests
+        String tag_json_obj = "jobj_req";
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+    }
+
+    private void postRequestAll() {
+        String url = "http://coms-309-sb-4.misc.iastate.edu:8080/updateuser";
+        url = url + "/" + sessionManager.getID() + "/";
+
+        Map<String, String> params = new HashMap<>();
+        if(!passwordEditText.getText().toString().equals("")){
+            params.put("Password", passwordEditText.getText().toString()); //password to register
+        }
+        if(!currentPassword.getText().toString().equals("")){
+            params.put("CurrentPassword", currentPassword.getText().toString());
+        }
+        if(!userEmailEditText.getText().toString().equals("")){
+            params.put("Email", userEmailEditText.getText().toString()); //email to register
+        }
+        if(!userNameEditText.getText().toString().equals("")){
+            params.put("Name", userNameEditText.getText().toString()); //name to register
+        }
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        try {
+                            String success = response.getString("Response");
+                            Toast.makeText(UserSettingsActivity.this, success, Toast.LENGTH_SHORT).show();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
