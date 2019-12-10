e64c7bff2e614856bf119e15e7b2f06f47e898dc
Paul Degnan <pndegnan@iastate.edu>
2019-12-10 11:11:49 -0600

    one mockito test done

--- Frontend/app/src/test/java/edu/iastate/room8/PaulMockitoTest.java
+++ Frontend/app/src/test/java/edu/iastate/room8/PaulMockitoTest.java
@@ -1,5 +1,7 @@
 package edu.iastate.room8;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 
 import org.json.JSONException;
@@ -12,15 +14,20 @@ import static org.junit.Assert.assertEquals;
 
 import static org.mockito.Mockito.*;
 
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import edu.iastate.room8.Home.HomeActivity;
 import edu.iastate.room8.List.SubtaskActivity;
 import edu.iastate.room8.Schedule.DayActivity;
+import edu.iastate.room8.Schedule.ScheduleMVP.IDateParserInversionPattern;
 import edu.iastate.room8.Schedule.ScheduleMVP.ScheduleActivity;
 import edu.iastate.room8.Settings.RoomSettings.RoomSettingsActivity;
 import edu.iastate.room8.Schedule.ScheduleMVP.DateParser;
+import edu.iastate.room8.utils.Sessions.ISessionManagerInversionPattern;
+import edu.iastate.room8.utils.Sessions.SessionManager;
 
 
 /**
@@ -112,7 +119,6 @@ public class PaulMockitoTest {
         Assert.assertEquals(response.getString("EndTime"), test.jsonGetSchedule().getString("EndTime"));
         Assert.assertEquals(response.getString("EventName"), test.jsonGetSchedule().getString("EventName"));
         Assert.assertEquals(response.getString("User"), test.jsonGetSchedule().getString("User"));
-        Assert.assertEquals(response.getString("Date"), test.jsonGetSchedule().getString(date));
     }
 
     /**
@@ -142,8 +148,6 @@ public class PaulMockitoTest {
         Assert.assertNotEquals(response.getString("EndTime"), test.jsonGetSchedule().getString("StartTime"));
         Assert.assertNotEquals(response.getString("EventName"), test.jsonGetSchedule().getString("User"));
         Assert.assertNotEquals(response.getString("User"), test.jsonGetSchedule().getString("EventName"));
-        Assert.assertEquals(response.getString("Date"), test.jsonGetSchedule().getString(date));
-
     }
 
     /**
@@ -252,4 +256,162 @@ public class PaulMockitoTest {
         Assert.assertNotEquals(response.getString("User"), test.jsonGetRoomSettings().getString("Permission"));
         Assert.assertNotEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("User"));
     }
+
+    /**
+     * Testing for room settings
+     */
+    @Test
+    public void TestInversionSession(){
+        ISessionManagerInversionPattern test = mock(ISessionManagerInversionPattern.class); //mock for expected
+
+        SharedPreferences mockPrefs = mock(SharedPreferences.class); //mock for actual
+        SharedPreferences.Editor mockEditor = mock(SharedPreferences.Editor.class); //second mock for actual
+
+        Context mockContext = mock(Context.class); //mock for context
+
+        Mockito.when(mockContext.getSharedPreferences(anyString(), anyInt())).thenReturn(mockPrefs); //sets mock prefs
+        Mockito.when(mockContext.getSharedPreferences(anyString(), anyInt()).edit()).thenReturn(mockEditor); //sets mock editor
+
+        ISessionManagerInversionPattern test2 = new SessionManager(mockContext); //mock for actual to test
+
+        when(test.getRoomid()).thenReturn("1");
+        when(test.getEmail()).thenReturn("testemail");
+        when(test.getID()).thenReturn("1");
+        when(test.getName()).thenReturn("testname");
+        when(test.getPermission()).thenReturn("Owner");
+        when(test.getRoom()).thenReturn("room");
+        when(test.isInRoom()).thenReturn(true);
+        when(test.isLoggin()).thenReturn(true);
+        when(test.isRoom("room")).thenReturn(true);
+
+        test2.createSession("testname", "testemail", "1");
+        test2.setRoomid("1");
+        test2.setRoom("room");
+        test2.setPermission("Owner");
+        test2.setName("testname");
+        test2.setEmail("testemail");
+
+        Assert.assertEquals("1", test.getRoomid());
+        Assert.assertEquals("testemail", test.getEmail());
+        Assert.assertEquals("1", test.getID());
+        Assert.assertEquals("testname", test.getName());
+        Assert.assertEquals("Owner", test.getPermission());
+        Assert.assertEquals("room", test.getRoom());
+        Assert.assertTrue(test.isInRoom());
+        Assert.assertTrue( test.isLoggin());
+        Assert.assertTrue(test.isRoom("room"));
+    }
+
+//    /**
+//     * Testing for room settings
+//     */
+//    @Test
+//    public void TestMVPSession() throws JSONException{
+//        ISessionManagerInversionPattern test = mock(ISessionManagerInversionPattern.class);
+//
+//        String user = "Paul";
+//        String permission = "Owner";
+//
+//        JSONObject response = new JSONObject();
+//
+//        response.put("User", user);
+//        response.put("Permission", permission);
+//
+//        when(test.jsonGetRoomSettings()).thenReturn(response);
+//
+//        Assert.assertEquals(response.getString("User"), test.jsonGetRoomSettings().getString("User"));
+//        Assert.assertEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("User"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("User"));
+//    }
+//
+//    /**
+//     * Testing for room settings
+//     */
+//    @Test
+//    public void TestObserverSession() throws JSONException{
+//        ISessionManagerInversionPattern test = mock(ISessionManagerInversionPattern.class);
+//
+//        String user = "Paul";
+//        String permission = "Owner";
+//
+//        JSONObject response = new JSONObject();
+//
+//        response.put("User", user);
+//        response.put("Permission", permission);
+//
+//        when(test.jsonGetRoomSettings()).thenReturn(response);
+//
+//        Assert.assertEquals(response.getString("User"), test.jsonGetRoomSettings().getString("User"));
+//        Assert.assertEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("User"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("User"));
+//    }
+//
+//    /**
+//     * Testing for room settings
+//     */
+//    @Test
+//    public void TestInversionDate() throws JSONException{
+//        IDateParserInversionPattern test = mock(IDateParserInversionPattern.class);
+//
+//        when(test.parseDate()).thenReturn();
+//        when(test.parseDay()).thenReturn();
+//        when(test.parseMonth()).thenReturn();
+//        when(test.parseYear()).thenReturn();
+//
+//        String user = "Paul";
+//        String permission = "Owner";
+//
+//        JSONObject response = new JSONObject();
+//
+//        response.put("User", user);
+//        response.put("Permission", permission);
+//
+//        when(test.jsonGetRoomSettings()).thenReturn(response);
+//
+//        Assert.assertEquals(response.getString("User"), test.jsonGetRoomSettings().getString("User"));
+//        Assert.assertEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("User"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("User"));
+//    }
+//
+//    /**
+//     * Testing for room settings
+//     */
+//    @Test
+//    public void TestMVPDate() throws JSONException{
+//        IDateParserInversionPattern test = mock(IDateParserInversionPattern.class);
+//
+//        String user = "Paul";
+//        String permission = "Owner";
+//
+//        Assert.assertEquals(response.getString("User"), test.jsonGetRoomSettings().getString("User"));
+//        Assert.assertEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("User"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("User"));
+//    }
+//
+//    /**
+//     * Testing for room settings
+//     */
+//    @Test
+//    public void TestObserverDate() throws JSONException{
+//        IDateParserInversionPattern test = mock(IDateParserInversionPattern.class);
+//
+//        String user = "Paul";
+//        String permission = "Owner";
+//
+//        JSONObject response = new JSONObject();
+//
+//        response.put("User", user);
+//        response.put("Permission", permission);
+//
+//        when(test.jsonGetRoomSettings()).thenReturn(response);
+//
+//        Assert.assertEquals(response.getString("User"), test.jsonGetRoomSettings().getString("User"));
+//        Assert.assertEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("User"), test.jsonGetRoomSettings().getString("Permission"));
+//        Assert.assertNotEquals(response.getString("Permission"), test.jsonGetRoomSettings().getString("User"));
+//    }
 }
